fe40632da2c1d43b19519a65f36b21c23710f0e1
Andres Rodriguez <andrudeluxe@gmail.com>
2026-04-09 18:50:14 -0500

    feat: implement DateInput component, add Spinner for loading state, and enhance ActivityForm with new fields and submission logic

--- src/features/activities/forms/ActivityForm.tsx
+++ src/features/activities/forms/ActivityForm.tsx
@@ -13,36 +13,85 @@ import {
 import TextInput from "@/components/forms/TextInput"
 import { ActivityRequestSchema, type ActivityRequest } from "../schemas/request/ActivityRequest"
 import type { ActivityResponse } from "@activities/schemas/response/ActivityResponse"
-import { useUpdateActivity } from "@activities/hooks/api/useActivities"
+import { useCreateActivity, useUpdateActivity } from "@activities/hooks/api/useActivities"
 import { toast } from "sonner"
-
+import { useEffect, useMemo } from "react"
+import { Spinner } from "@/components/ui/spinner"
+import { AddSquareIcon, Edit01Icon } from "@hugeicons/core-free-icons"
+import { HugeiconsIcon } from "@hugeicons/react"
+import DateInput from "@/components/forms/DateInput"
 interface Props {
   activity: ActivityResponse
 }
 
 export default function ActivityForm({ activity }: Props) {
-  const { updateActivityAsync } = useUpdateActivity()
+  const { updateActivityAsync, isPendingUpdateActivity } = useUpdateActivity()
+  const { createActivityAsync, isPendingCreateActivity } = useCreateActivity()
+
   const form = useForm({
     resolver: zodResolver(ActivityRequestSchema),
     defaultValues: activity,
   })
 
+  useEffect(() => {
+    if (activity) {
+      form.reset(activity)
+    }
+  }, [activity, form])
+
+  const {
+    formState: { isValid },
+  } = form
+
   async function onSubmit(data: ActivityRequest) {
-    await updateActivityAsync(data, {
-      onSuccess: () => {
-        toast.success("Activity updated successfully")
-        form.reset()
-      },
-      onError: error => {
-        toast.error(`Error updating the activity ${error.message}`)
-      },
-    })
+    console.log(data)
+
+    if (activity) {
+      await updateActivityAsync(data, {
+        onSuccess: () => {
+          toast.success("Activity updated successfully")
+          form.reset()
+        },
+        onError: error => {
+          toast.error(`Error updating the activity ${error.message}`)
+        },
+      })
+    } else {
+      await createActivityAsync(data, {
+        onSuccess: () => {
+          toast.success("Activity created successfully")
+          form.reset()
+        },
+        onError: error => {
+          toast.error(`Error creating the activity ${error.message}`)
+        },
+      })
+    }
   }
+
+  const isSubmitting = useMemo(() => {
+    return isPendingUpdateActivity || isPendingCreateActivity
+  }, [isPendingUpdateActivity, isPendingCreateActivity])
+
+  const isDisabled = useMemo(() => {
+    return isPendingUpdateActivity || isPendingCreateActivity || !isValid
+  }, [isPendingUpdateActivity, isPendingCreateActivity, !isValid])
 
   return (
     <Card className="w-full sm:max-w-md">
       <CardHeader>
-        <CardTitle>Create Activity</CardTitle>
+        <CardTitle className="flex gap-2">
+          {activity ? (
+            <>
+              <HugeiconsIcon icon={Edit01Icon} className="text-primary" /> Update
+            </>
+          ) : (
+            <>
+              <HugeiconsIcon icon={AddSquareIcon} className="text-primary" /> Create
+            </>
+          )}{" "}
+          Activity
+        </CardTitle>
         <CardDescription>Help us improve by reporting bugs you encounter.</CardDescription>
       </CardHeader>
       <CardContent>
@@ -61,14 +110,44 @@ export default function ActivityForm({ activity }: Props) {
             rows={3}
             placeholder="Enter a detailed description"
           />
+          <TextInput
+            label="Category"
+            control={form.control}
+            name="category"
+            placeholder="Enter activity category"
+          />
+          <DateInput
+            label="Date"
+            control={form.control}
+            name="date"
+            placeholder="Enter activity date"
+          />
+          <TextInput
+            label="Venue"
+            control={form.control}
+            name="venue"
+            placeholder="Enter activity venue"
+          />
+          <TextInput
+            label="City"
+            control={form.control}
+            name="city"
+            placeholder="Enter activity city"
+          />
         </form>
       </CardContent>
       <CardFooter className="flex gap-2">
         <Button type="button" variant="outline" onClick={() => form.reset()}>
           Reset
         </Button>
-        <Button type="submit" form="form-rhf-demo">
-          Submit
+        <Button type="submit" form="form-rhf-demo" disabled={isDisabled}>
+          {isSubmitting ? (
+            <>
+              <Spinner /> Submitting
+            </>
+          ) : (
+            "Submit"
+          )}
         </Button>
       </CardFooter>
     </Card>
--- src/features/activities/schemas/request/ActivityRequest.ts
+++ src/features/activities/schemas/request/ActivityRequest.ts
@@ -2,16 +2,15 @@ import { requiredString } from "@/lib/utils"
 import { z } from "zod"
 
 export const ActivityRequestSchema = z.object({
+  id: z.string(),
   title: requiredString("Title"),
   description: requiredString("Description"),
   category: requiredString("Category"),
   date: z.coerce.date(),
-  location: z.object({
-    venue: requiredString("Venue"),
-    city: z.string(),
-    latitude: z.coerce.number(),
-    longitude: z.coerce.number(),
-  }),
+  venue: requiredString("Venue"),
+  city: z.string(),
+  // latitude: z.coerce.number(),
+  // longitude: z.coerce.number(),
 })
 
 export type ActivityRequest = z.infer<typeof ActivityRequestSchema>
