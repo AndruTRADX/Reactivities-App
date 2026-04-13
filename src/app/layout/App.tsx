27b9accdfbb8df99090089ef38faeea4d2f34425
Andres Rodriguez <andrudeluxe@gmail.com>
2026-04-13 17:15:16 -0500

    feat: enhance layout and styling of activity and home pages for improved UI consistency

--- src/app/layout/App.tsx
+++ src/app/layout/App.tsx
@@ -7,8 +7,10 @@ function App() {
     <div>
       <Navbar />
       <ConfirmDialog />
-      <main className="pt-17 pb-6 px-5.5 bg-primary-foreground min-h-dvh">
-        <Outlet />
+      <main className="flex justify-center w-full pt-17 pb-6 px-5.5 bg-primary-foreground min-h-dvh">
+        <div className="w-full max-w-7xl gap-3 mt-4">
+          <Outlet />
+        </div>
       </main>
     </div>
   )
--- src/features/home/HomePage.tsx
+++ src/features/home/HomePage.tsx
@@ -6,11 +6,11 @@ import { Link } from "react-router"
 
 export default function HomePage() {
   return (
-    <div className="flex items-center justify-center min-h-dvh">
-      <Card className="w-full max-w-md -mt-40">
+    <div className="flex items-center justify-center h-full">
+      <Card className="w-full max-w-md -mt-20">
         <CardHeader className="text-center">
           <CardTitle className="flex justify-center items-center gap-2 text-4xl font-bold">
-            <HugeiconsIcon icon={Activity} size={40} className="text-primary" />
+            <HugeiconsIcon icon={Activity} size={44} className="text-primary" />
             Reactivities</CardTitle>
           <CardDescription className="text-base mt-3">
             Discover and join activities in your community. Connect with others and make memorable experiences.
