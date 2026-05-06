e217f0fc4c28ed51b0927784e9ad0539f1933863
Andres Rodriguez <andrudeluxe@gmail.com>
2026-05-06 13:51:10 -0500

    feat: add LocationIQ types and implement LocationInput component for location selection

--- src/features/activities/schemas/request/ActivityRequest.ts
+++ src/features/activities/schemas/request/ActivityRequest.ts
@@ -2,13 +2,19 @@ import { requiredString } from "@/shared/lib/utils"
 import { z } from "zod"
 
 export const ActivityRequestSchema = z.object({
-  id: z.string().optional().nullable(),
+  id: z.string().optional(),
   title: requiredString("Title"),
   description: requiredString("Description"),
   category: requiredString("Category"),
-  date: z.coerce.date(),
-  venue: requiredString("Venue"),
-  city: requiredString("City"),
+  date: z.coerce.date({
+    error: "Date is required",
+  }),
+  location: z.object({
+    venue: requiredString("Venue"),
+    city: z.string().optional(),
+    latitude: z.coerce.number(),
+    longitude: z.coerce.number(),
+  }),
   // latitude: z.coerce.number(),
   // longitude: z.coerce.number(),
 })
