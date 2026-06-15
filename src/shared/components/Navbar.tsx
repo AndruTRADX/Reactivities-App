85d638d712f671302a8a9a23f3aa4581a9efa5b1
Andres Rodriguez <andrudeluxe@gmail.com>
2026-06-15 14:42:39 -0500

    feat: add UserResponse schema and implement useGetCurrentUser hook for user data retrieval; enhance Navbar with user profile display

--- src/shared/components/Navbar.tsx
+++ src/shared/components/Navbar.tsx
@@ -1,22 +1,25 @@
-import { UserSearch } from "@hugeicons/core-free-icons"
+import { UserCircleIcon, UserSearch } from "@hugeicons/core-free-icons"
 import { HugeiconsIcon } from "@hugeicons/react"
 import { Button } from "@sharedUi/button"
 import { NavLink } from "react-router"
 import { cn } from "../lib/utils"
+import { useGetCurrentUser } from "../hooks/api/useAccount"
 
 export default function Navbar() {
+  const { user } = useGetCurrentUser()
+
   return (
     <nav className="z-50 fixed w-full flex justify-between px-5.5 py-2.5 bg-primary-foreground/50 backdrop-blur-xs">
       <NavLink to="/" end>
         {({ isActive }) => (
           <Button
-            size='lg'
-            variant='ghost'
+            size="lg"
+            variant="ghost"
             className={cn(
               `flex items-center gap-1 cursor-pointer ${isActive ? "text-primary hover:text-primary/80" : "text-foreground"}`
             )}
           >
-            <HugeiconsIcon icon={UserSearch} strokeWidth={2}/>
+            <HugeiconsIcon icon={UserSearch} strokeWidth={2} />
             <h3 className="font-semibold text-base">Reactivities</h3>
           </Button>
         )}
@@ -46,7 +49,16 @@ export default function Navbar() {
         </NavLink>
       </div>
 
-      <Button>User menu</Button>
+      {user ? (
+        <div className="flex gap-4 items-center">
+          <Button variant="outline">
+            <HugeiconsIcon icon={UserCircleIcon} className="text-primary min-w-5" />
+            {user.displayName}'s Profile
+          </Button>
+        </div>
+      ) : (
+        <Button>Sign In</Button>
+      )}
     </nav>
   )
 }
